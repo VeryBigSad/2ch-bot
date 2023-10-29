9b032b084022347c982b75f8b3d76112faaa8887
Mikhail Khromov <khromov05@gmail.com>
2023-10-29 23:14:46 +0300

    fix typo

--- handlers/users/source.py
+++ handlers/users/source.py
@@ -7,5 +7,5 @@ from data.config import SOURCE_URL
 @dp.message_handler(commands="source")
 async def source_command(message: types.Message):
     await message.answer(
-        f"Исходник бота: {SOURCE_URL}\nПоставь звездочку пжпж (если знаешь как) \<3"
+        f"Исходник бота: {SOURCE_URL}\nПоставь звездочку пжпж (если знаешь как!)"
     )
