86ed809483c22e5e20c9be544b21cd1840405a32
Mikhail Khromov <khromov05@gmail.com>
2023-10-29 23:07:18 +0300

    fix name

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,7 +9,7 @@ async def start_command(message: types.Message):
     try:
         await commands.add_user(int(message.from_user.id))
         await message.answer(
-            "Добро пожаловать в sch58 2ch! Все твои сообщения анонимны.\n/enter чтобы зайти в чат\n/help чтобы узнать команды"
+            "Добро пожаловать в MISIS 2ch! Все твои сообщения анонимны.\n/enter чтобы зайти в чат\n/help чтобы узнать команды"
         )
     except Exception:
         print("User adding error")
